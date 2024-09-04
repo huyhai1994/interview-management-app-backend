3ebdf80b2d2fe578178924b6f351d6d317c1a982
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-09-04 20:53:18 +0700

    security start

---
+++ src/main/java/com/codegym/interviewmanagementapp/InterviewManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.codegym.interviewmanagementapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewManagementAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterviewManagementAppApplication.class, args);
+    }
+
+}
